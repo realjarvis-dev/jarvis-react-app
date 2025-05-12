077d4740aa0312cb5da5d3a38d322e27b511061e
Rui Zhu <thestral2017@gmail.com>
2025-05-12 12:00:00 +0800

    change: env var name

--- lib/alchemy/client.ts
+++ lib/alchemy/client.ts
@@ -2,7 +2,7 @@ import { Alchemy, Network } from "alchemy-sdk";
 
 const config = {
   apiKey: process.env.ALCHEMY_API_KEY!,
-  network: process.env.TEST_NET_ENV === 'development' ? Network.ETH_SEPOLIA : Network.ETH_MAINNET,
+  network: process.env.NEXT_PUBLIC_TEST_NET_ENV === 'development' ? Network.ETH_SEPOLIA : Network.ETH_MAINNET,
   connectionInfoOverrides: {
     // <-- disable the internal referrer setup that Next.js 14’s undici fetch chokes on
     skipFetchSetup: true,
