79475173ac7d51e98fb1e676e843d312673ec8a5
Rui Zhu <thestral2017@gmail.com>
2025-05-12 12:58:48 +0800

    fix: mobile layout on /

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -14,8 +14,8 @@ const exampleMessages: {
   iconAriaLabel: string
 }[] = [
   {
-    heading: "Explore yield opportunities on Pendle",
-    message: "List the yielding opportunities on Pendle",
+    heading: 'Explore yield opportunities on Pendle',
+    message: 'List the yielding opportunities on Pendle',
     icon: Globe2,
     iconAriaLabel: 'Web search'
   },
@@ -46,26 +46,31 @@ export function EmptyScreen({
   className?: string
 }) {
   return (
-    <div className={`mx-auto w-full transition-all ${className}`}>
-      <div className="bg-background p-2">
-        <div className="mt-2 flex flex-col items-start space-y-2 mb-4">
+    <div className={`w-full transition-all min-h-[150px] ${className}`}>
+      <div className="bg-background p-0 sm:p-0.5 md:p-2 lg:p-4">
+        <div className="mt-0 sm:mt-0.5 md:mt-2 flex flex-col items-start space-y-0.5 sm:space-y-1 md:space-y-2 mb-0.5 sm:mb-1 md:mb-4">
           {exampleMessages.map((message, index) => (
             <Button
               key={index}
               variant="link"
-              className="h-auto p-0 text-base"
+              className="h-auto p-0 text-[9px] sm:text-xs md:text-sm lg:text-base whitespace-normal"
               name={message.message}
               onClick={async () => {
                 submitMessage(message.message)
               }}
             >
-              <ArrowRight size={16} className="mr-2 text-muted-foreground" />
+              <ArrowRight
+                size={10}
+                className="mr-0.5 sm:mr-1 md:mr-2 text-muted-foreground shrink-0 min-w-[10px]"
+              />
               <message.icon
-                size={16}
-                className="mr-2 text-muted-foreground"
+                size={10}
+                className="mr-0.5 sm:mr-1 md:mr-2 text-muted-foreground shrink-0 min-w-[10px]"
                 aria-label={message.iconAriaLabel}
               />
-              {message.heading}
+              <span className="text-left truncate max-w-[220px] sm:max-w-none">
+                {message.heading}
+              </span>
             </Button>
           ))}
         </div>
