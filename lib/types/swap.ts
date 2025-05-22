b134b51a1fc921ad928bf0068f6a9cbc9e79b672
Rui Zhu <thestral2017@gmail.com>
2025-05-22 22:19:31 +0800

    modify: use enso's swap for pendle pt token swap

---
+++ lib/types/swap.ts
@@ -0,0 +1,42 @@
+/** 
+ * @param chainId Chain ID
+ * @param tokenIn Address of the input token
+ * @param tokenOut Address of the output token
+ * @param fromAddress Address of the sender
+ * @param amountIn Amount of input token in wei
+ * @param slippage Slippage tolerance (e.g., 0.01 for 1%)
+ */
+export type EnsoSwapInput = {
+    chainId: number,
+    tokenIn: string
+    tokenOut: string,
+    fromAddress: string,
+    amountIn: string,
+    slippage: number
+}
+
+/** 
+ * @param tokenOut Address of the output token
+ * @param fromAddress Address of the sender
+ * @param amountIn Amount of input token in wei
+ * @param slippage Slippage tolerance (e.g., 0.01 for 1%)
+ */
+export type EnsoSwapETHToPTInput = {
+    tokenOut: string,
+    fromAddress: string,
+    amountIn: string,
+    slippage: number
+}
+
+/**
+ * @param data Transaction data
+ * @param to Address of the recipient
+ * @param from Address of the sender
+ * @param value Value of the transaction in wei
+ */
+export type EnsoTxInput = {
+    data: string,
+    to: string,
+    from: string,
+    value: string
+}
