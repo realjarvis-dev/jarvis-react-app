618c9b8d052d5a6e8c62edeb3240115501be9532
gaurav219688 <gaurav@kirastudio.xyz>
2025-05-23 20:11:19 +0530

    add: kodiak testing route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
+import { getAccessToken, usePrivy } from "@privy-io/react-auth";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { getAccessToken, usePrivy } from "@privy-io/react-auth";
-import Head from "next/head";
 
 async function verifyToken() {
   const url = "/api/verify";
@@ -77,12 +77,18 @@ export default function DashboardPage() {
 
   return (
     <>
-
-
       <main className="flex flex-col min-h-screen px-4 sm:px-20 py-6 sm:py-10 bg-privy-light-blue">
         {ready && authenticated ? (
           <>
             <div className="flex flex-row justify-between">
+              <div className="flex gap-2">
+                <Link 
+                  href="/kodiak-test"
+                  className="text-sm bg-blue-200 hover:text-blue-900 py-2 px-4 rounded-md text-blue-700"
+                >
+                  Kodiak Test
+                </Link>
+              </div>
               <button
                 onClick={logout}
                 className="text-sm bg-violet-200 hover:text-violet-900 py-2 px-4 rounded-md text-violet-700"
