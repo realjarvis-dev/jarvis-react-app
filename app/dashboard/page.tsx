5d3272b5a8958d1a045cbb5af92120591761ee65
gaurav219688 <gaurav@kirastudio.xyz>
2025-05-24 21:48:57 +0530

    removed: kodiak test route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { getAccessToken, usePrivy } from "@privy-io/react-auth";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -80,22 +79,6 @@ export default function DashboardPage() {
       <main className="flex flex-col min-h-screen px-4 sm:px-20 py-6 sm:py-10 bg-privy-light-blue">
         {ready && authenticated ? (
           <>
-            <div className="flex flex-row justify-between">
-              <div className="flex gap-2">
-                <Link 
-                  href="/kodiak-test"
-                  className="text-sm bg-blue-200 hover:text-blue-900 py-2 px-4 rounded-md text-blue-700"
-                >
-                  Kodiak Test
-                </Link>
-              </div>
-              <button
-                onClick={logout}
-                className="text-sm bg-violet-200 hover:text-violet-900 py-2 px-4 rounded-md text-violet-700"
-              >
-                Logout
-              </button>
-            </div>
             <div className="mt-12 flex gap-4 flex-wrap">
               {googleSubject ? (
                 <button
