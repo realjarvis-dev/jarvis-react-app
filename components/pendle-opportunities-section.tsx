302e7c21ed2899fafa9b8ca37ae4311c8962e1c7
gaurav219688 <gaurav@kirastudio.xyz>
2025-05-26 11:37:29 +0530

    update: UI to use the pendle table display

--- components/pendle-opportunities-section.tsx
+++ components/pendle-opportunities-section.tsx
@@ -1,7 +1,8 @@
-import { OpportunityCard } from './opportunity-card'
-import { DefaultSkeleton } from './default-skeleton'
-import { ToolArgsSection } from './section'
 import { CollapsibleMessage } from './collapsible-message'
+import { DefaultSkeleton } from './default-skeleton'
+import { PendleOpportunitiesTable } from './pendle-opportunities-table'
+import { ToolArgsSection } from './section'
+
 interface PendleOpportunitiesSectionProps {
   tool: any
   isOpen: boolean
@@ -30,14 +31,10 @@ export function PendleOpportunitiesSection({
     header={header}
     isOpen={isOpen}
     onOpenChange={onOpenChange}
-    showIcon={false} // Assuming we want an icon
+    showIcon={false}
   >
       {results.length === 0 && <div>No opportunities found.</div>}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {results.map((opp: any, i: number) => (
-          <OpportunityCard key={i} {...opp} />
-        ))}
-      </div>
+      <PendleOpportunitiesTable opportunities={results} />
     </CollapsibleMessage>
   )
 }
