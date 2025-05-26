91373d2dbe1d2e8cd2c2ed52acabf3e7bb5b86d6
gaurav219688 <gaurav@kirastudio.xyz>
2025-05-26 16:38:34 +0530

    remove: model reference

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 export const dynamic = 'force-dynamic'
 
 import { Chat } from '@/components/chat'
-import { getModels } from '@/lib/config/models'
 import { generateId } from 'ai'
-import { cookies, headers } from 'next/headers'
+import { cookies } from 'next/headers'
 
 export default async function Page() {
   const id = generateId()
-  const models = await getModels()
   const cookiesList = await cookies()
 
-  return <Chat id={id} models={models} />
+  return <Chat id={id} />
 }
