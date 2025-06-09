4a0b4111e56bc662292391087c2cab99a8088b58
Rui Zhu <thestral2017@gmail.com>
2025-06-09 17:41:07 +0800

    remove: unused prefetch

--- lib/hooks/use-usd-balance.ts
+++ lib/hooks/use-usd-balance.ts
@@ -8,25 +8,6 @@ import { useEffect } from 'react'
 export function useUsdBalance() {
     const { selectedChain, isDemoMode, allChainTypes } = useNetwork()
     const { user, ready, authenticated } = usePrivy()
-    //TODO: write a new endpoint that uses input chainId and isDemoMode instead of reading from cookies
-    // const queryClient = useQueryClient()
-    // // pre-fetch all chain types
-    // useEffect(() => {
-    //     allChainTypes.forEach(chainId => {
-    //       if (chainId !== selectedChain) {
-    //         queryClient.prefetchQuery(
-    //           {queryKey: ['usdBalance', chainId, isDemoMode, user?.id, ready, authenticated],
-    //             queryFn: () => fetch('/api/wallet/token-usd-price')
-    //                     .then(r => r.json())
-    //                     .then(d => d.usdBalance),
-    //             staleTime: 600_000,
-    //             gcTime: 600_000
-    //         }
-    //         )
-    //       }
-    //     })
-    //   }, [allChainTypes, selectedChain, isDemoMode, queryClient, user?.id, ready, authenticated])
-
     const { data, isLoading, error } = useQuery({
         queryKey: ['usdBalance', selectedChain, isDemoMode, user?.id, ready, authenticated],
         queryFn: () => {
