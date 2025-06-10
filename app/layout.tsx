1e1244db794e3aee08eab380f7a849ffb6cb998e
Rui Zhu <thestral2017@gmail.com>
2025-06-10 16:40:21 +0800

    add: overflow constraints

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,17 +74,18 @@ export default async function RootLayout({
                     with ArtifactProvider */}
                   <ArtifactProvider>
                     <AppSidebar />
+                    <div className="flex flex-col flex-1 min-w-0">
                     <Header />
                     <div
-                      className="flex flex-col flex-1 overflow-hidden pt-[56px] px-4 sm:px-6 z-25"
+                      className="flex flex-col flex-1 overflow-hidden pt-[56px] px-4 sm:px-6"
                       style={{
                         paddingTop: `calc(56px + env(safe-area-inset-top))`
                       }}
                     >
-                      <main className="flex-1 w-full overflow-auto">
+                      <main className="flex-1 w-full min-w-0 overflow-y-auto overflow-x-hidden">
                         <ArtifactRoot>{children}</ArtifactRoot>
                       </main>
-      
+                    </div>
                     </div>
                   </ArtifactProvider>
                 </SidebarProvider>
