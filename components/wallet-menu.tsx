edd020e96ce20b954e41c94b8461041a3da160db
Rui Zhu <thestral2017@gmail.com>
2025-06-11 15:47:09 +0800

    add: make it to be return to mainpage when on wallet page

--- components/wallet-menu.tsx
+++ components/wallet-menu.tsx
@@ -1,30 +1,27 @@
 'use client'
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
-
-import { useWallets } from '@privy-io/react-auth'
-import { Info, Wallet } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import { CopyableWalletAddress } from './copyable-wallet-address'
+import { ArrowLeft, Wallet } from 'lucide-react'
+import { usePathname, useRouter } from 'next/navigation'
 import { Button } from './ui/button'
 
 export default function WalletMenu() {
   const router = useRouter()
-  const { wallets } = useWallets()
-  const wallet = wallets[0]
+  const pathname = usePathname()
+
+  const isWalletPage = pathname === '/wallet'
 
   return (
-        <Button variant="ghost" 
-        size="icon" 
-        className="ring-0 outline-none focus:ring-0 focus:outline-none focus:shadow-none active:ring-0 active:outline-none active:shadow-none bg-transparent hover:bg-transparent active:bg-transparent rounded-md"
-        onClick={() => router.push('/wallet')}>
-          <Wallet className="w-4 h-4" />
-        </Button>
+    <Button
+      variant="ghost"
+      size="icon"
+      className="ring-0 outline-none focus:ring-0 focus:outline-none focus:shadow-none active:ring-0 active:outline-none active:shadow-none bg-transparent hover:bg-transparent active:bg-transparent rounded-md"
+      onClick={() => router.push(isWalletPage ? '/' : '/wallet')}
+    >
+      {isWalletPage ? (
+        <ArrowLeft className="w-4 h-4" />
+      ) : (
+        <Wallet className="w-4 h-4" />
+      )}
+    </Button>
   )
 }
