4e29613126488d236f2c1c8cb5860f21b9da2318
Rui Zhu <thestral2017@gmail.com>
2025-06-18 23:34:19 +0800

    remove: console log

--- lib/pendle/liquidity-single.ts
+++ lib/pendle/liquidity-single.ts
@@ -14,13 +14,11 @@ async function formatOutputAndExecute<T>(res: MethodReturnType<T>, chainId: numb
     }
     console.log(res)
     if (res.tokenApprovals) {
-        console.log("tokenApprovals", res.tokenApprovals)
         const tokenApprovals = res.tokenApprovals.map((tokenApproval) => {
             
             return erc20Approval(tokenApproval.token, res.tx.to, tokenApproval.amount, userAddress, chainId, isDemo);
         });
         const tx = await Promise.all(tokenApprovals);
-        console.log("tx", tx)
     }
     // Send tx
     try {
