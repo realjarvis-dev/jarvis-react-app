6ed246bcf2d61c08e52386c8f17bc953713f6f40
gaurav219688 <gaurav@kirastudio.xyz>
2025-07-08 22:19:13 +0530

    Update types.ts

--- lib/streaming/types.ts
+++ lib/streaming/types.ts
@@ -1,13 +1,14 @@
 import { WalletWithMetadata } from '@privy-io/server-auth'
 import { Message } from 'ai'
-import { Model } from '../types/models'
 import { NetworkContext } from '../types/context'
+import { Model } from '../types/models'
 
 export interface BaseStreamConfig {
   messages: Message[]
   model: Model
   chatId: string
   searchMode: boolean,
+  deepResearchMode: boolean,
   userId: string
   userEvmWallet: WalletWithMetadata | undefined
   userSolWallet: WalletWithMetadata | undefined
