bee1fd0dab6ab00e46fb4d60593a0ec6eca98410
Rui Zhu <thestral2017@gmail.com>
2025-07-16 22:44:55 +0800

    add: socket url logging

--- lib/pubsub/socket.ts
+++ lib/pubsub/socket.ts
@@ -1,4 +1,6 @@
 // lib/socket.ts
 import { io } from 'socket.io-client';
 const socket = io(process.env.PRICE_ALERT_INTERNAL_URL || "http://localhost:3002");
+
+console.log("socket url", process.env.PRICE_ALERT_INTERNAL_URL)
 export default socket;
