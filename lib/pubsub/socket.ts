e493c4a56330bf098a821864c3b4b095d1e1ad65
Rui Zhu <thestral2017@gmail.com>
2025-07-17 15:12:47 +0800

    update: use deployed url

--- lib/pubsub/socket.ts
+++ lib/pubsub/socket.ts
@@ -1,6 +1,4 @@
 // lib/socket.ts
 import { io } from 'socket.io-client';
-const socket = io(process.env.NEXT_PUBLIC_PRICE_ALERT_INTERNAL_URL);
-
-console.log("socket url", process.env.NEXT_PUBLIC_PRICE_ALERT_INTERNAL_URL)
+const socket = io(process.env.NEXT_PUBLIC_TEST_NET_ENV === 'development' ? "http://localhost:3002" : "https://jarvis-alert.onrender.com");
 export default socket;
