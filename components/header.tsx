9ad9ee6181897ec089bc0eb535a44364167abc29
Rui Zhu <thestral2017@gmail.com>
2025-07-18 18:51:01 +0800

    add: inbox and fetch notifications from redis

--- components/header.tsx
+++ components/header.tsx
@@ -15,6 +15,7 @@ import { ArrowLeft } from 'lucide-react'
 import { usePathname, useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 import GuestMenu from './guest-menu'
+import NotificationInbox from './notification-inbox'
 import UserMenu from './user-menu'
 import WalletMenu from './wallet-menu'
 import WelcomePopup from './welcome-popup'
@@ -100,6 +101,7 @@ export const Header: React.FC = () => {
           </Button>
         )}
         {ready && authenticated && <WalletMenu />}
+        {ready && authenticated && <NotificationInbox />}
         {ready && authenticated ? <UserMenu /> : <GuestMenu login={login} />}
       </div>
     </header>
