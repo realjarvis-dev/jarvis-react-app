f2d0f713380d3b86d60eef359488a6841bbb9716
Rui Zhu <thestral2017@gmail.com>
2025-07-28 22:16:29 +0800

    add: next config for fixing path import for inngest

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: [
+    "@inngest/workflow-kit",
+    "jsonpath"
+  ],
   images: {
     remotePatterns: [
       {
